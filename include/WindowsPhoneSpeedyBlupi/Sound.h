c25b6820bf14e9707b63b31f2a79c65626022013
Robert Vokac <robertvokac@robertvokac.com>
2025-05-24 16:35:06 +0200

    Work in progress #4

--- include/WindowsPhoneSpeedyBlupi/Sound.h
+++ include/WindowsPhoneSpeedyBlupi/Sound.h
@@ -33,31 +33,9 @@ namespace WindowsPhoneSpeedyBlupi {
     public:NeoSdk::Property<bool> IsFree;
 
 
-    public: Play(Microsoft::Xna::Framework::Audio::SoundEffect& se, int channel, double volume, double balance, double* pitch, bool isLooped):
+    public: Play(Microsoft::Xna::Framework::Audio::SoundEffect& se, int channel, double volume, double balance, double pitch, bool isLooped);
 
-        Channel( [channel]() { return channel; }),
-        IsFree( [*this]() { return sei.State == Microsoft::Xna::Framework::Audio::SoundState::Stopped; }),
-        channel(channel),
-        sei(se.CreateInstance())
-            {
-                int num = channel * 2;
-                if (num >= 0 && num < tableVolumePitchLength)
-                {
-                    volume *= tableVolumePitch[num];
-                    *pitch = tableVolumePitch[num + 1];
-                }
-
-                sei.Volume = (float)volume;
-                sei.Pan = (float)balance;
-                sei.Pitch = (float)(pitch == nullptr ? 0.0 : *pitch);
-                sei.IsLooped = isLooped;
-                sei.Play();
-            }
-
-    public :void Stop()
-            {
-                sei.Stop();
-            }
+    public: void Stop();
         };
 
     private: static constexpr short tableVolumePitchLength = 200;
@@ -97,177 +75,43 @@ namespace WindowsPhoneSpeedyBlupi {
 
     private: double volume;
 
-    public: Sound(Game1& game1, GameData& gameData):
-        game1(game1),
-        gameData(gameData)
-        {
+    public: Sound(Game1& game1, GameData& gameData);
 
-            // soundEffects = new List<SoundEffect>();
-            // plays = new List<Play>();
-            volume = 1.0;
-            Microsoft::Xna::Framework::Audio::SoundEffect::MasterVolume = 1.0f;
-        }
 
     public:
-        void LoadContent()
-        {
-            if (Def::HasSound)
-            {
-                for (int i = 0; i <= 92; i++)
-                {
-                    std::ostringstream oss;
-                    oss << "sounds/sound" << std::setw(3) << std::setfill('0') << i;
-                    std::string assetName = oss.str();
+        void LoadContent();
 
-                    Microsoft::Xna::Framework::Audio::SoundEffect item = game1.Content.get().Load<Microsoft::Xna::Framework::Audio::SoundEffect>(assetName);
-                    soundEffects.push_back(item);
-                }
-            }
-        }
+        bool Create();
 
-     bool Create()
-        {
-            return true;
-        }
+        void SetState(bool bState);
 
-         void SetState(bool bState)
-        {
-        }
+        void SetCDAudio(bool bAudio);
 
-         void SetCDAudio(bool bAudio)
-        {
-        }
+        bool GetEnable();
 
-         bool GetEnable()
-        {
-            return true;
-        }
+        void SetAudioVolume(int volume);
 
-         void SetAudioVolume(int volume)
-        {
-            this.volume = (double)volume / (double)MAXVOLUME;
-        }
+        int GetAudioVolume();
 
-         int GetAudioVolume()
-        {
-            return (int)(volume * (double)MAXVOLUME);
-        }
+        void SetMidiVolume(int volume);
 
-         void SetMidiVolume(int volume)
-        {
-        }
+        int GetMidiVolume();
 
-         int GetMidiVolume()
-        {
-            return 0;
-        }
+        void StopAll();
 
-         void StopAll()
-        {
-            while (plays.Any())
-            {
-                plays[0].Stop();
-                plays.RemoveAt(0);
-            }
-        }
+        bool PlayImage(int channel, TinyPoint& pos);
 
-         bool PlayImage(int channel, TinyPoint pos)
-        {
-            return PlayImage(channel, pos, -1, false);
-        }
+        bool PlayImage(int channel, TinyPoint& pos, int rank, bool bLoop);
 
-         bool PlayImage(int channel, TinyPoint pos, int rank, bool bLoop)
-        {
-            if (!gameData.Sounds)
-            {
-                return true;
-            }
-            if (channel >= 0 && channel < soundEffects.Count)
-            {
-                if (channel != 10 && plays.Where((Play x) => x.Channel == channel && !x.IsFree).Any())
-                {
-                    return true;
-                }
-                if (plays.Count >= 10)
-                {
-                    int num = 0;
-                    while (num < plays.Count)
-                    {
-                        if (plays[num].IsFree)
-                        {
-                            plays.RemoveAt(num);
-                        }
-                        else
-                        {
-                            num++;
-                        }
-                    }
-                }
-                Play item = new Play(soundEffects[channel], channel, (float)GetVolume(pos), (float)GetBalance(pos), null, bLoop);
-                plays.Add(item);
-            }
-            return true;
-        }
+        bool PosImage(int channel, TinyPoint& pos);
 
-         bool PosImage(int channel, TinyPoint pos)
-        {
-            return true;
-        }
-
-         bool Stop(int channel)
-        {
-            int num = 0;
-            while (num < plays.Count)
-            {
-                if (plays[num].Channel == channel)
-                {
-                    plays[num].Stop();
-                    plays.RemoveAt(num);
-                }
-                else
-                {
-                    num++;
-                }
-            }
-            return true;
-        }
+        bool Stop(int channel);
 
     private:
-        double GetVolume(TinyPoint pos)
-        {
-            double val = 1.0;
-            if (pos.X < 0)
-            {
-                val = 1.0 + (double)(pos.X / 640) * 2.0;
-            }
-            if (pos.X > 640)
-            {
-                pos.X -= 640;
-                val = 1.0 - (double)(pos.X / 640) * 2.0;
-            }
-            val = std::max(val, 0.0);
-            val = std::min(val, 1.0);
-            double val2 = 1.0;
-            if (pos.Y < 0)
-            {
-                val2 = 1.0 + (double)(pos.Y / 480) * 3.0;
-            }
-            if (pos.Y > 480)
-            {
-                pos.Y -= 480;
-                val2 = 1.0 - (double)(pos.Y / 480) * 3.0;
-            }
-            val2 = std::max(val2, 0.0);
-            val2 = std::min(val2, 1.0);
-            return std::min(val, val2) * volume;
-        }
+        double GetVolume(TinyPoint& pos);
+        double GetBalance(TinyPoint& pos);
 
-         double GetBalance(TinyPoint pos)
-        {
-            double val = (double)pos.X * 2.0 / 640.0 - 1.0;
-            val = std::max(val, -1.0);
-            return std::min(val, 1.0);
-        }
+
     };
 }
 
--- src/WindowsPhoneSpeedyBlupi/Sound.cpp
+++ src/WindowsPhoneSpeedyBlupi/Sound.cpp
@@ -3,3 +3,194 @@
 //
 
 #include "WindowsPhoneSpeedyBlupi/Sound.h"
+namespace WindowsPhoneSpeedyBlupi {
+    Sound::Play::Play(Microsoft::Xna::Framework::Audio::SoundEffect& se, int channel, double volume, double balance, double pitch, bool isLooped):
+
+        Channel( [channel]() { return channel; }),
+        IsFree( [*this]() { return sei.State == Microsoft::Xna::Framework::Audio::SoundState::Stopped; }),
+        channel(channel),
+        sei(se.CreateInstance())
+    {
+        int num = channel * 2;
+        if (num >= 0 && num < tableVolumePitchLength)
+        {
+            volume *= tableVolumePitch[num];
+            pitch = tableVolumePitch[num + 1];
+        }
+
+        sei.Volume = (float)volume;
+        sei.Pan = (float)balance;
+        sei.Pitch = (float)(pitch < 0.0 ? 0.0: pitch);
+        sei.IsLooped = isLooped;
+        sei.Play();
+    }
+    void Sound::Play::Stop()
+    {
+        sei.Stop();
+    }
+
+    Sound::Sound(Game1& game1, GameData& gameData):
+    game1(game1),
+    gameData(gameData)
+    {
+
+        // soundEffects = new List<SoundEffect>();
+        // plays = new List<Play>();
+        volume = 1.0;
+        Microsoft::Xna::Framework::Audio::SoundEffect::MasterVolume = 1.0f;
+    }
+
+
+        void Sound::LoadContent()
+        {
+            if (Def::HasSound)
+            {
+                for (int i = 0; i <= 92; i++)
+                {
+                    std::ostringstream oss;
+                    oss << "sounds/sound" << std::setw(3) << std::setfill('0') << i;
+                    std::string assetName = oss.str();
+
+                    Microsoft::Xna::Framework::Audio::SoundEffect item = game1.Content.get().Load<Microsoft::Xna::Framework::Audio::SoundEffect>(assetName);
+                    soundEffects.push_back(item);
+                }
+            }
+        }
+
+     bool Sound::Create()
+        {
+            return true;
+        }
+
+         void Sound::SetState(bool bState)
+        {
+        }
+
+         void Sound::SetCDAudio(bool bAudio)
+        {
+        }
+
+         bool Sound::GetEnable()
+        {
+            return true;
+        }
+
+         void Sound::SetAudioVolume(int volume)
+        {
+            this->volume = (double)volume / (double)MAXVOLUME;
+        }
+
+         int Sound::GetAudioVolume()
+        {
+            return (int)(volume * (double)MAXVOLUME);
+        }
+
+         void Sound::SetMidiVolume(int volume)
+        {
+        }
+
+         int Sound::GetMidiVolume()
+        {
+            return 0;
+        }
+
+         void Sound::StopAll()
+        {
+            while (!plays.empty())
+            {
+                plays[0].Stop();
+                plays.erase(plays.begin());
+            }
+        }
+
+         bool Sound::PlayImage(int channel, TinyPoint& pos)
+        {
+            return PlayImage(channel, pos, -1, false);
+        }
+
+         bool Sound::PlayImage(int channel, TinyPoint& pos, int rank, bool bLoop)
+        {
+            if (!gameData.Sounds)
+            {
+                return true;
+            }
+            if (channel >= 0 && channel < soundEffects.size())
+            {
+
+                if (channel != 10 && std::any_of(plays.begin(), plays.end(),
+    [channel](const Play& p) { return p.Channel == channel && !p.IsFree; })) {
+                    return true;
+    }
+
+                if (plays.size() >= 10) {
+                    plays.erase(std::remove_if(plays.begin(), plays.end(),
+                        [](const Play& p) { return p.IsFree; }), plays.end());
+                }
+
+                plays.emplace_back(soundEffects[channel], channel, (float)GetVolume(pos), (float)GetBalance(pos), 0.0, bLoop);
+
+            }
+            return true;
+        }
+
+         bool Sound::PosImage(int channel, TinyPoint& pos)
+        {
+            return true;
+        }
+
+         bool Sound::Stop(int channel)
+        {
+            size_t num = 0;
+            while (num < plays.size())
+            {
+                if (plays[num].Channel == channel)
+                {
+                    plays[num].Stop();
+                    plays.erase(plays.begin() + num);
+                }
+                else
+                {
+                    num++;
+                }
+            }
+            return true;
+        }
+
+
+        double Sound::GetVolume(TinyPoint& pos)
+        {
+            double val = 1.0;
+            if (pos.X < 0)
+            {
+                val = 1.0 + (double)(pos.X / 640) * 2.0;
+            }
+            if (pos.X > 640)
+            {
+                pos.X -= 640;
+                val = 1.0 - (double)(pos.X / 640) * 2.0;
+            }
+            val = std::max(val, 0.0);
+            val = std::min(val, 1.0);
+            double val2 = 1.0;
+            if (pos.Y < 0)
+            {
+                val2 = 1.0 + (double)(pos.Y / 480) * 3.0;
+            }
+            if (pos.Y > 480)
+            {
+                pos.Y -= 480;
+                val2 = 1.0 - (double)(pos.Y / 480) * 3.0;
+            }
+            val2 = std::max(val2, 0.0);
+            val2 = std::min(val2, 1.0);
+            return std::min(val, val2) * volume;
+        }
+
+         double Sound::GetBalance(TinyPoint& pos)
+        {
+            double val = (double)pos.X * 2.0 / 640.0 - 1.0;
+            val = std::max(val, -1.0);
+            return std::min(val, 1.0);
+        }
+
+}
