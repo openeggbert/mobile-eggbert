aec7c256c205d2806f2b42c1d3071f32d6af7041
Robert Vokac <robertvokac@robertvokac.com>
2025-05-31 16:47:10 +0200

    Rewriting the class Decor to C++ #5

--- include/WindowsPhoneSpeedyBlupi/Helper.h
+++ include/WindowsPhoneSpeedyBlupi/Helper.h
@@ -11,12 +11,12 @@
 
 namespace WindowsPhoneSpeedyBlupi {
     class Helper {
-    public: static std::string formatString(const std::string& format, const std::vector<std::string>& args);
-        static std::vector<std::string> split(const std::string& text, char delimiter);
+    public:
+        static std::string formatString(const std::string &format, const std::vector<std::string> &args);
 
+        static std::vector<std::string> split(const std::string &text, char delimiter);
     };
 }
-
 
 
 #endif //HELPER_H
