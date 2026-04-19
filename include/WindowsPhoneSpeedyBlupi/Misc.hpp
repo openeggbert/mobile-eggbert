55be1e8f0944e3b48b5fe986898d70af126d2509
Robert Vokac <robertvokac@robertvokac.com>
2026-04-19 20:02:14 +0200

    Misc was updated

--- include/WindowsPhoneSpeedyBlupi/Misc.hpp
+++ include/WindowsPhoneSpeedyBlupi/Misc.hpp
@@ -1,49 +1,192 @@
-//
-// Created by robertvokac on 5/24/25.
-//
+#pragma once
 
-#ifndef MISC_H
-#define MISC_H
 #include "TinyPoint.hpp"
 #include "TinyRect.hpp"
 #include "Microsoft/Xna/Framework/Rectangle.hpp"
+#include "CppDotNet/CppDotNetHelper.hpp"
 
-namespace WindowsPhoneSpeedyBlupi {
+namespace WindowsPhoneSpeedyBlupi
+{
+    using CppDotNet::intcs;
+    using WindowsPhoneSpeedyBlupi::TinyPoint;
 
-using WindowsPhoneSpeedyBlupi::TinyPoint;
-//static class
+    /**
+     * @brief Provides miscellaneous helper methods used by the game.
+     *
+     * This class is a C++ port of the original C# static class Misc from
+     * WindowsPhoneSpeedyBlupi.
+     *
+     * It contains utility methods for point rotation, angle conversion,
+     * rectangle operations, and small numeric helpers.
+     *
+     * @note Status: Ported
+     */
     class Misc
     {
     public:
-        static Microsoft::Xna::Framework::Rectangle RotateAdjust(const Microsoft::Xna::Framework::Rectangle& rect, const double &angle);
+        /**
+         * @brief Deleted constructor because this is a static utility class.
+         *
+         * @note Status: Ported
+         */
+        Misc() = delete;
 
-        static double DegToRad(const double &angle);
+        /**
+         * @brief Deleted destructor because this is a static utility class.
+         *
+         * @note Status: Ported
+         */
+        ~Misc() = delete;
 
-        static TinyPoint RotatePointRad(const TinyPoint& center, const double& angle, const TinyPoint& point);
+        /**
+         * @brief Adjusts a rectangle position after rotation around its center.
+         *
+         * The returned rectangle keeps the same width and height as the input
+         * rectangle, but its top-left position is adjusted according to the
+         * rotation of the rectangle center.
+         *
+         * @param rect Rectangle to adjust.
+         * @param angle Rotation angle in radians.
+         * @return Adjusted rectangle.
+         *
+         * @note Status: Ported
+         */
+        [[nodiscard]] static Microsoft::Xna::Framework::Rectangle RotateAdjust(
+            const Microsoft::Xna::Framework::Rectangle& rect,
+            double angle);
 
-         static TinyPoint RotatePointRad(const double& angle, const TinyPoint& p);
+        /**
+         * @brief Rotates a point around the origin using an angle in radians.
+         *
+         * This overload rotates the point around (0, 0).
+         *
+         * @param angle Rotation angle in radians.
+         * @param p Point to rotate.
+         * @return Rotated point.
+         *
+         * @note Status: Ported
+         */
+        [[nodiscard]] static TinyPoint RotatePointRad(double angle, const TinyPoint& p);
 
+        /**
+         * @brief Rotates a point around a specified center using an angle in radians.
+         *
+         * @param center Center of rotation.
+         * @param angle Rotation angle in radians.
+         * @param point Point to rotate.
+         * @return Rotated point.
+         *
+         * @note Status: Ported
+         */
+        [[nodiscard]] static TinyPoint RotatePointRad(
+            const TinyPoint& center,
+            double angle,
+            const TinyPoint& point);
 
+        /**
+         * @brief Converts an angle from degrees to radians.
+         *
+         * @param angle Angle in degrees.
+         * @return Angle in radians.
+         *
+         * @note Status: Ported
+         */
+        [[nodiscard]] static double DegToRad(double angle);
 
+        /**
+         * @brief Moves an integer value toward a target value by a given step.
+         *
+         * If @p actual is smaller than @p final, it is increased by @p step
+         * but not beyond @p final. If @p actual is greater than @p final,
+         * it is decreased by @p step but not below @p final.
+         *
+         * @param actual Current value.
+         * @param final Target value.
+         * @param step Step size.
+         * @return Adjusted value.
+         *
+         * @note Status: Ported
+         */
+        [[nodiscard]] static intcs Approach(intcs actual, intcs final, intcs step);
 
+        /**
+         * @brief Converts a normalized speed factor into an integer speed.
+         *
+         * Positive speeds return at least 1, negative speeds return at most -1,
+         * and zero returns 0.
+         *
+         * @param speed Speed factor.
+         * @param max Maximum magnitude scaling factor.
+         * @return Integer speed.
+         *
+         * @note Status: Ported
+         */
+        [[nodiscard]] static intcs Speed(double speed, intcs max);
 
-         static int Approach(int actual, const int &final, const int &step);
+        /**
+         * @brief Inflates a TinyRect by the specified value in all directions.
+         *
+         * @param rect Rectangle to inflate.
+         * @param value Amount to subtract from left/top and add to right/bottom.
+         * @return Inflated rectangle.
+         *
+         * @note Status: Ported
+         */
+        [[nodiscard]] static TinyRect Inflate(const TinyRect& rect, intcs value);
 
-         static int Speed(const double& speed, const int& max);
+        /**
+         * @brief Returns true if a point lies inside or on the border of a rectangle.
+         *
+         * @param rect Rectangle to test.
+         * @param p Point to test.
+         * @return True if the point is inside the rectangle; otherwise false.
+         *
+         * @note Status: Ported
+         */
+        [[nodiscard]] static bool IsInside(const TinyRect& rect, const TinyPoint& p);
 
-         static TinyRect Inflate(const TinyRect& rect, const int& value);
-
-         static bool IsInside(const TinyRect& rect, const TinyPoint& p);
-
+        /**
+         * @brief Computes the intersection of two rectangles.
+         *
+         * The destination rectangle is filled with the overlapping area of
+         * @p src1 and @p src2.
+         *
+         * @param dst Output rectangle receiving the intersection.
+         * @param src1 First source rectangle.
+         * @param src2 Second source rectangle.
+         * @return True if the resulting rectangle is non-empty; otherwise false.
+         *
+         * @note Status: Ported
+         */
         static bool IntersectRect(TinyRect& dst, const TinyRect& src1, const TinyRect& src2);
 
+        /**
+         * @brief Computes the union of two rectangles.
+         *
+         * The destination rectangle is filled with the bounding rectangle
+         * covering both @p src1 and @p src2.
+         *
+         * @param dst Output rectangle receiving the union.
+         * @param src1 First source rectangle.
+         * @param src2 Second source rectangle.
+         * @return True if the resulting rectangle is non-empty; otherwise false.
+         *
+         * @note Status: Ported
+         */
         static bool UnionRect(TinyRect& dst, const TinyRect& src1, const TinyRect& src2);
 
     private:
-        static bool IsRectEmpty(const TinyRect& rect);
-
+        /**
+         * @brief Returns true if the specified rectangle is empty.
+         *
+         * A rectangle is considered empty if its width is less than or equal to zero
+         * or its height is less than or equal to zero.
+         *
+         * @param rect Rectangle to test.
+         * @return True if the rectangle is empty; otherwise false.
+         *
+         * @note Status: Ported
+         */
+        [[nodiscard]] static bool IsRectEmpty(const TinyRect& rect);
     };
-
 }
-
-#endif //MISC_H
--- src/WindowsPhoneSpeedyBlupi/Misc.cpp
+++ src/WindowsPhoneSpeedyBlupi/Misc.cpp
@@ -1,87 +1,91 @@
-//
-// Created by robertvokac on 5/24/25.
-//
-
 #include "WindowsPhoneSpeedyBlupi/Misc.hpp"
-#include <cmath>
 
-// WindowsPhoneSpeedyBlupi, Version=1.0.0.5, Culture=neutral, PublicKeyToken=6db12cd62dbec439
-// WindowsPhoneSpeedyBlupi.Misc
-// using System;
-// using System.Diagnostics;
-// using Microsoft.Xna.Framework;
-// using WindowsPhoneSpeedyBlupi;
-// using static WindowsPhoneSpeedyBlupi.Def;
+#include "System/Math.hpp"
 
 namespace WindowsPhoneSpeedyBlupi
 {
-
-    Microsoft::Xna::Framework::Rectangle Misc::RotateAdjust(const Microsoft::Xna::Framework::Rectangle& rect, const double &angle)
+    Microsoft::Xna::Framework::Rectangle Misc::RotateAdjust(const Microsoft::Xna::Framework::Rectangle& rect,
+                                                            const double angle)
     {
-        TinyPoint tinyPoint;
-        tinyPoint.X = rect.Width / 2;
-        tinyPoint.Y = rect.Height / 2;
-        TinyPoint p = tinyPoint;
-        TinyPoint tinyPoint2 = RotatePointRad(angle, p);
-        int num = tinyPoint2.X - p.X;
-        int num2 = tinyPoint2.Y - p.Y;
-        return {rect.getLeftProperty() - num, rect.getTopProperty() - num2, rect.Width, rect.Height};
-    }
-    double Misc::DegToRad(const double &angle)
-    {
-        return angle * M_PI / 180.0;
-    }
-
-    TinyPoint Misc::RotatePointRad(const TinyPoint& center, const double& angle, const TinyPoint& point)
-    {
-        TinyPoint tinyPoint;
-        TinyPoint result;
-        tinyPoint.X = point.X - center.X;
-        tinyPoint.Y = point.Y - center.Y;
-        double rad = DegToRad(angle);
-        double num = sin(rad);
-        double num2 = cos(rad);
-        result.X = (int)((double)tinyPoint.X * num2 - (double)tinyPoint.Y * num);
-        result.Y = (int)((double)tinyPoint.X * num + (double)tinyPoint.Y * num2);
-        result.X += center.X;
-        result.Y += center.Y;
-        return result;
-    }
-    TinyPoint Misc::RotatePointRad(const double& angle, const TinyPoint& p)
-    {
-        return RotatePointRad(TinyPoint(), angle, p);
+        TinyPoint center{};
+        center.X = rect.Width / 2;
+        center.Y = rect.Height / 2;
+        TinyPoint originalCenter = center;
+        TinyPoint rotatedCenter = RotatePointRad(angle, originalCenter);
+        int offsetX = rotatedCenter.X - originalCenter.X;
+        int offsetY = rotatedCenter.Y - originalCenter.Y;
+        return {
+            rect.getLeftProperty() - offsetX,
+            rect.getTopProperty() - offsetY,
+            rect.Width,
+            rect.Height
+        };
     }
 
-    int Misc::Approach(int actual, const int& final, const int& step)
+    TinyPoint Misc::RotatePointRad(double angle, const TinyPoint& p)
+    {
+        return RotatePointRad(TinyPoint{}, angle, p);
+    }
+
+    TinyPoint Misc::RotatePointRad(const TinyPoint& center, double angle, const TinyPoint& point)
+    {
+        TinyPoint relativePoint{};
+        TinyPoint rotatedPoint{};
+
+        relativePoint.X = point.X - center.X;
+        relativePoint.Y = point.Y - center.Y;
+
+        double sinAngle = System::Math::Sin(angle);
+        double cosAngle = System::Math::Cos(angle);
+
+        rotatedPoint.X = static_cast<int>(
+            static_cast<double>(relativePoint.X) * cosAngle -
+            static_cast<double>(relativePoint.Y) * sinAngle);
+
+        rotatedPoint.Y = static_cast<int>(
+            static_cast<double>(relativePoint.X) * sinAngle +
+            static_cast<double>(relativePoint.Y) * cosAngle);
+
+        rotatedPoint.X += center.X;
+        rotatedPoint.Y += center.Y;
+
+        return rotatedPoint;
+    }
+
+    double Misc::DegToRad(double angle)
+    {
+        return angle * System::Math::PI / 180.0;
+    }
+
+    intcs Misc::Approach(intcs actual, const intcs final, const intcs step)
     {
         if (actual < final)
         {
-            actual = std::min(actual + step, final);
+            actual = System::Math::Min(actual + step, final);
         }
         else if (actual > final)
         {
-            actual = std::max(actual - step, final);
+            actual = System::Math::Max(actual - step, final);
         }
         return actual;
-
     }
 
-    int Misc::Speed(const double& speed, const int& max)
+    intcs Misc::Speed(const double speed, const intcs max)
     {
         if (speed > 0.0)
         {
-            return std::max((int)(speed * (double)max), 1);
+            return System::Math::Max(static_cast<intcs>(speed * static_cast<double>(max)), 1);
         }
         if (speed < 0.0)
         {
-            return std::min((int)(speed * (double)max), -1);
+            return System::Math::Min(static_cast<intcs>(speed * static_cast<double>(max)), -1);
         }
         return 0;
     }
 
-    TinyRect Misc::Inflate(const TinyRect& rect, const int& value)
+    TinyRect Misc::Inflate(const TinyRect& rect, const intcs value)
     {
-        TinyRect result;
+        TinyRect result{};
         result.Left = rect.Left - value;
         result.Right = rect.Right + value;
         result.Top = rect.Top - value;
@@ -95,21 +99,21 @@ namespace WindowsPhoneSpeedyBlupi
 
     bool Misc::IntersectRect(TinyRect& dst, const TinyRect& src1, const TinyRect& src2)
     {
-        dst = TinyRect();
-        dst.Left = std::max(src1.Left, src2.Left);
-        dst.Right = std::min(src1.Right, src2.Right);
-        dst.Top = std::max(src1.Top, src2.Top);
-        dst.Bottom = std::min(src1.Bottom, src2.Bottom);
+        dst = TinyRect{};
+        dst.Left = System::Math::Max(src1.Left, src2.Left);
+        dst.Right = System::Math::Min(src1.Right, src2.Right);
+        dst.Top = System::Math::Max(src1.Top, src2.Top);
+        dst.Bottom = System::Math::Min(src1.Bottom, src2.Bottom);
         return !IsRectEmpty(dst);
     }
 
     bool Misc::UnionRect(TinyRect& dst, const TinyRect& src1, const TinyRect& src2)
     {
         dst = TinyRect();
-        dst.Left = std::min(src1.Left, src2.Left);
-        dst.Right = std::max(src1.Right, src2.Right);
-        dst.Top = std::min(src1.Top, src2.Top);
-        dst.Bottom = std::max(src1.Bottom, src2.Bottom);
+        dst.Left = System::Math::Min(src1.Left, src2.Left);
+        dst.Right = System::Math::Max(src1.Right, src2.Right);
+        dst.Top = System::Math::Min(src1.Top, src2.Top);
+        dst.Bottom = System::Math::Max(src1.Bottom, src2.Bottom);
         return !IsRectEmpty(dst);
     }
 
